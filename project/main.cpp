7c0ac642aa13907f2ebe9241a283c61e8892e38c
MrSofty123 <shaheer1642@gmail.com>
2022-06-21 23:31:02 +0500

    basics

---
+++ project/main.cpp
@@ -0,0 +1,117 @@
+/* 
+IMPORTANT NOTE: 
+
+	Add the following line to Tools > Compiler Options > General:
+		-std=c++11 
+		
+	or else the program will not compile
+*/
+
+#include <iostream>         // includring for cout/cin
+#include <fstream>          // including for file handling
+#include <iomanip>          // including for setw() function
+#include <conio.h>          // including for getch() function
+#include <regex>
+#include "lib/json.hpp"     // including for JSON data structure support
+
+using namespace std;
+using json = nlohmann::json;        // defining json as data type
+
+string strUpper(string str) {       // this function converts a given string to upper case
+    string upper;
+    // loop through each character and convert to upper case using toupper() built-in function
+    for (int x=0; x<str.length(); x++) {
+        upper += toupper(str[x]);
+    }
+    
+    return upper;
+}
+string strLower(string str) {       // this function converts a given string to lower case
+    string upper;
+    // loop through each character and convert to lower case using tolower() built-in function
+    for (int x=0; x<str.length(); x++) {
+        upper += tolower(str[x]);
+    }
+    
+    return upper;
+}
+string replaceSpecialChars(string str) {
+    str = regex_replace(str, regex("\\?"), "");
+    str = regex_replace(str, regex("\\."), "");
+    str = regex_replace(str, regex("\\!"), "");
+    str = regex_replace(str, regex("\\'"), "");
+    return str;
+}
+
+// the Currency class that contains data about each currency and some methods
+class Bot {
+	private:
+    	json botData;      // contains data for upto 150 currency rates
+    public:
+        string name;    // bot's name
+
+    public: 
+	    Bot() {        // the constructor method to iniatialize class variables
+            // assign random name to the bot
+            string namesArr[] = {"AlbertOne","A.L.I.C.E.","Charlix","Cleverbot","EugeneGoostman","Fred","Jabberwacky","JeeneyAI","Lenny","MegaHAL","SimSimi","Spookitalk","UltraHal","Verbot"};
+            srand(time(NULL));
+            name = namesArr[rand() % 13];
+
+            // get file contents
+	        string fileContent;
+	        string line;
+	        ifstream readFile("data/qna.json");     // this file contains json formatted data about currency rates
+            // read each line and append to fileContent variable
+	        while (getline (readFile, line)) {     // read the file line by line and append to fileContent var
+	            fileContent += line;
+	        }
+            // parsing/converting the read string into json data structure
+	        botData = json::parse(fileContent);
+            return;
+	    }
+
+        string getAnswer(string input) {
+            input = replaceSpecialChars(strLower(input));
+            json answers;
+            for (int i=0; i<this->botData.size(); i++) {
+                string question = replaceSpecialChars(strLower(this->botData[i]["question"]));
+                if (question == input)
+                    answers.push_back(this->name + ": " + to_string(this->botData[i]["answer"]));
+            }
+            if (answers.size() > 0) {
+                srand(time(NULL));
+                return answers[rand() % answers.size()];
+            } else {
+                return (this->name + ": Sorry, I did not understand.");
+            }
+        }
+};
+Bot* bot = new Bot();
+
+int initializeConversation() {
+    cout<<endl<<"You: ";
+    string input;
+    getline(cin, input);
+    if (strLower(input) == "bye") {
+        cout<<bot->name<<": It was fun talking. Good bye!";
+        return 0;
+    } else {
+        cout<<bot->getAnswer(input)<<endl;
+        initializeConversation();       // get input recursively until user types 'bye'
+    }
+}
+
+int printWelcomeScreen() {
+    cout<<"Welcome to Simple Chatbot"<<endl;
+    cout<<"-------------------------\nType 'bye' to exit program.\n\n";
+    cout<<bot->name<<": "<<"Hey there, how may I help you?"<<endl;
+
+    initializeConversation();
+    return 0;
+}
+
+int main() {
+    printWelcomeScreen();    // call main menu until it returns 0
+    delete bot;
+    return 0;
+}
